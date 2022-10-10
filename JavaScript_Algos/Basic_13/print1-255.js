46c0640d35c01557ebdb4f033ed85756474972f6
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-10 12:34:37 -0700

    Basic 13 #1

---
+++ JavaScript_Algos/Basic_13/print1-255.js
@@ -0,0 +1,18 @@
+// Do-While loop
+let i = 0;
+do{
+    i++
+    console.log(i);
+}
+while(i < 255);
+
+//While loop
+let x = 1;
+while(x < 256){
+    console.log(x);
+    x++;
+}
+// For Loop
+for(let i = 1; i < 256; i++){
+    console.log(i);
+}
