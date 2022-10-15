7c1cbe00887f21fef8aee1a9da2853ad20e75a0e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-14 19:55:16 -0700

    Complete Threes Fives pt1 | JS fundamentals

---
+++ JavaScript_Algos/Fundamentals/threesAndFives.js
@@ -0,0 +1,20 @@
+//Create function ​ThreesFives()​ that adds each value from 100 and 4000000 (inclusive) if that value is evenly divisible by 3 or 5 ​but not both​. Display the final sum in the console.
+//Second:​ Change your function to make a ​BetterThreesFives(start,end)​where ​start​ and ​end values are customizable. You can think of the above ​ThreesFives()​ function as BetterThreesFives(100,4000000)​.
+
+function ThreesFives(){
+    let i = 100;
+    let sum = 0;
+    while (i < 4000000){
+        if (i % 3 == 0 || i % 5 == 0){
+            if (i % 3 == 0 && i % 5 == 0){
+                // console.log('Not adding'+i);
+            } else {
+                sum += i;
+            }
+        }
+        i++
+    }
+    console.log(sum);
+}
+
+ThreesFives()
