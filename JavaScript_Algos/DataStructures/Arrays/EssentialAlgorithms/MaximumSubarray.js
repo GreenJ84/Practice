8108842d48be650526e7739f03156c6ec60e5b95
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-20 10:25:40 -0700

    completed Maximum Subarray | Leetcode JS Algo

---
+++ JavaScript_Algos/DataStructures/Arrays/EssentialAlgorithms/MaximumSubarray.js
@@ -0,0 +1,16 @@
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// A subarray is a contiguous part of an array.
+
+function maxSubArray(array){
+    let maxSum = array[0];
+    let sum = 0;
+
+    for (let num in array){
+        sum += parseInt(array[num]);
+        maxSum = Math.max(maxSum, sum);
+        sum = Math.max(0, sum)
+    }
+    return maxSum;
+}
+
+maxSubArray([-2,1,-3,4,-1,2,1,-5,4]);
