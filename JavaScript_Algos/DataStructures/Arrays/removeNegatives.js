fb84180d1e43d5412d4affdb4112c3c805d494f3
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-20 09:30:54 -0700

    Completed Remove negatives Array algorithm

---
+++ JavaScript_Algos/DataStructures/Arrays/removeNegatives.js
@@ -0,0 +1,22 @@
+// Implement a function ​removeNegatives()​ that accepts an array and removes any values that are less than zero.
+// Second-level challenge: ​don’t use nested loops.
+
+function removeNegatives(array){
+    let i = 0;
+    while (i < array.length){
+        if (array[i] < 0){
+            if ( i === array.length-1){ array.pop() }
+
+            let j = i+1;
+            while(j < array.length){
+                array[j-1] = array[j];
+                j++;
+            }
+            array.pop()
+        }
+        i++;
+    }
+    console.log(array);
+    return array;
+}
+// removeNegatives([-1, 1, -2, 2, -3, 3]);
