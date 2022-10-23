61c79d010f342d0ae1edf96e20c1a5aefe817fc9
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-23 13:45:20 -0700

    Completed Second to Last | JS Array Algos

---
+++ JavaScript_Algos/DataStructures/Arrays/secondToLast.js
@@ -0,0 +1,9 @@
+// Return the second-to-last element of an array.
+
+function secondToLast(array){
+    return array[array.length-2];
+}
+
+console.log(secondToLast([1,2,3,4,5,6,7,8,9,10]));
+console.log(secondToLast([10,20,30,40,50,60,70,80,90,100]));
+console.log(secondToLast([1,7,35,79,4,56,-2,19,88]));
