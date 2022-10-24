1cc13a931248e231931b9d6e4861a85d15c59d48
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-24 11:51:08 -0700

    Moved Structures into dedicated folders

---
+++ JavaScript_Algos/DataStructures/Object/Objects.js
@@ -0,0 +1,2 @@
+// JavaScript Objects (dictionaries, maps, key-value pairs) are associative arrays. Think of them as arrays indexed by strings. Objects are initialized as {}, not [ ]. Access them as object attributes (​arr.attrib​) or object indices (​arr["index"]​).
+
