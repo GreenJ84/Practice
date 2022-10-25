3254e3101d4737c11b13cb838f84bcc2dc637134
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-25 15:04:42 -0700

    Completed Sum 3/5 to N | LeetCode TS Algo

---
+++ TypeScriptAlgos/sum3and5toN.ts
@@ -0,0 +1,20 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+//Create a function so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+const sum3and5toN = (number: number) => {
+    let sum: number = 0;
+    if (number < 0){ return sum }
+    let i: number = 3;
+    while (i < number){
+        if (i % 3 === 0 ){
+        sum += i;
+        } else if (i % 5 === 0){
+        sum += i;
+        }
+        i++;
+    }
+    return sum;
+}
+
+console.log(sum3and5toN(10));
