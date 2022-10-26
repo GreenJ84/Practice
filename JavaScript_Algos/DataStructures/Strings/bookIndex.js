5074dbb5a6c43021cc1129b8b0e614cbd3e7c8c4
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-26 13:14:19 -0700

    Completed Book Index | JS String Algo

---
+++ JavaScript_Algos/DataStructures/Strings/bookIndex.js
@@ -0,0 +1,1 @@
+// Write a function that given a sorted array of page numbers, return a string representing a book index. Combine consecutive pages to create ranges. Given ​[1, 3, 4, 5, 7, 8, 10]​, return the string ​"1, 3-5, 7-8, 10"​.
