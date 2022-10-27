df0b4ab94f9f55336dc1de8bc76281a80a4afd4c
Jesse Greenough <jesse.greenough84@gmail.com>
2022-10-27 09:51:51 -0700

    Completed Longest Substring | JS String Algo

---
+++ JavaScript_Algos/DataStructures/Strings/longestPalindrome.js
@@ -0,0 +1,2 @@
+// For this challenge, we will look not only at the entire string, but also substrings within it.
+// For a string, return the longest palindromic substring. Given ​"what up, dada?"​, return "dad"​. Given ​"not much"​, return ​"n"​. ​Include spaces​ as well (i.e. be strict, as in the “Is Palindrome” challenge): given ​"My favorite racecar erupted!"​, return ​"e racecar e"​.
