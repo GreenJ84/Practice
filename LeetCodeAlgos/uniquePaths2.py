e3ec705239b9a06e8c473c6105e932629b2b4d12
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-02 09:57:50 -0700

    Copleted all but few edge cases, Unique Paths 2

--- LeetCodeAlgos/uniquePaths2.py
+++ LeetCodeAlgos/uniquePaths2.py
@@ -4,41 +4,44 @@ class Solution(object):
         m = len(obGr)
         n = len(obGr[0])
         # Base Cases
-        if m == n <= 1:
+        if m < 1 | n < 1:
             return 0
+        if obGr[m-1][n-1] | obGr[0][0] == 1:
+            return 0
+        if m == n == 1:
+            return 1
         if m == 1:
-            if 1 not in n:
+            if 1 not in obGr[0]:
                 return 1
             else: 
                 return 0
-
         if n == 1:
             for i in obGr:
                 if 1 in i:
                     return 0
             return 1
 
-        #! Change all obsticles negative
         s = 0
         for i in obGr:
             obGr[s] = [ -1 if j > 0 else j for j in i]
             s+=1
 
-        #! Set edge Paths to 1 as the are unique by themselves and edges
-        # Set Endpoint to 1
         obGr[m-1][n-1] = 1
-        # Set Right edge to 1s
-        for item in range(n):
+
+        for item in range(n-1, -1, -1):
             print(obGr[m-1][item])
             if obGr[m-1][item] == 0:
                 obGr[m-1][item] = 1
-        # Set Bottom edge to 1s
-        for row in range(m):
+            elif obGr[m-1][item] == -1:
+                break
+
+        for row in range(m-1, -1, -1):
             print(obGr[row][n-1])
             if obGr[row][n-1] == 0:
                 obGr[row][n-1] = 1
+            elif obGr[row][n-1] == -1:
+                break
 
-        # From the second to last item in the second to last row, start to come backwards to the robot checking bottom and right adjacents
         for row in range(m-2, -1, -1):
             for item in range(n-2, -1, -1):
                 current = obGr[row][item]
@@ -46,16 +49,19 @@ class Solution(object):
                 right = obGr[row][item+1]
 
                 if current == 0:
-                    if bottom <= 0:
+                    if bottom & right <= 0:
+                        obGr[row][item] = -1
+                    elif bottom <= 0:
                         obGr[row][item] = right
                     elif right <= 0:
                         obGr[row][item] = bottom
                     else:
                         obGr[row][item] = bottom + right
 
-        # Return maximum of routes the robot can take when you get back to him
-        return obGr[0][0]
+        if obGr[0][0] > 0:
+            return obGr[0][0]
+        else: 
+            return 0
 
 solution = Solution()
-x = solution.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])
-print(x)
+print(solution.uniquePathsWithObstacles([[0,0],[1,1],[0,0]]))
