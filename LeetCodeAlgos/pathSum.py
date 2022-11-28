400c90254012bf576e25381b307dc6f82b9118ca
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-28 12:25:00 -0800

    Completed Path Sum | Leetcode Algo

---
+++ LeetCodeAlgos/pathSum.py
@@ -0,0 +1,21 @@
+# Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+# A leaf is a node with no children.
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        sum = targetSum
+        if not root:
+            return False
+        if not root.left and not root.right and root.val == sum:
+            return True
+        sum -= root.val
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
