289a1787ecadb627b2adbff0d1a1216f1cfb80df
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-19 12:32:50 -0800

    Completed Sort List | Leetcode Algo

---
+++ LeetCodeAlgos/sortList.py
@@ -0,0 +1,26 @@
+# Given the head of a linked list, return the list after sorting it in ascending order.
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: return head
+
+        vals = []
+        while head:
+            vals.append(head.val)
+            head=head.next
+        vals.sort()
+        setup = runner = ListNode()
+
+        for i in vals:
+            runner.next = ListNode(i)
+            runner = runner.next
+        return setup.next
+
