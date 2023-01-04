1e843e19899332b4a951fbdcc284f0ad004a1414
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-04 14:00:48 -0800

    Enhanced First Occurnace memory usage

--- LeetCodeAlgos/indexOfFirstoccuranceString.py
+++ LeetCodeAlgos/indexOfFirstoccuranceString.py
@@ -1,16 +1,17 @@
 # Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+#* 97% runtime/ 65% Memory
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         i = 0
         while i < len(haystack):
             if haystack[i] == needle[0]:
-                s, j, pos = 1, i+1, i
+                s, j = 1, i+1
                 while j < len(haystack) and s < len(needle) and haystack[j]==needle[s]:
                     s+=1
                     j+=1
                 if s == len(needle):
-                    return pos
+                    return i
             i+=1
         return -1
 
