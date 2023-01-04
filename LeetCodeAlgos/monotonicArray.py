15ebe23696cb0a14eecba1cd55f1dc4248f523cf
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-04 13:55:39 -0800

    Enhanced Monotomic Array solution

--- LeetCodeAlgos/monotonicArray.py
+++ LeetCodeAlgos/monotonicArray.py
@@ -4,24 +4,42 @@
 
 from typing import List
 
-
+#* 91% runtime/85% Meory
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        x = self.check(nums)
-        nums.reverse()
-        return (x or self.check(nums))
+        if nums[0]<nums[len(nums)-1]:
+            inc = True
+        else:
+            inc = False
+
+        for i in range(0, len(nums)-1):
+            if inc:
+                if not nums[i]<=nums[i+1]:
+                    return False
+            else:
+                if not nums[i]>=nums[i+1]:
+                    return False
+        return True
+
+#* Attempt 1: Passe with bad runtime
+# class Solution:
+#     def isMonotonic(self, nums: List[int]) -> bool:
+#         x = self.check(nums)
+#         nums.reverse()
+#         return (x or self.check(nums))
         
-    def check(self, nums):
-        inc, dec, i = True, True, 0
-        for j in range(len(nums)-1, 0, -1):
-            if nums[i] > nums[i+1]:
-                inc = False
-            if nums[j] < nums[j-1]:
-                dec = False
-            i+=1
-        return (inc or dec)
+#     def check(self, nums):
+#         inc, dec, i = True, True, 0
+#         for j in range(len(nums)-1, 0, -1):
+#             if nums[i] > nums[i+1]:
+#                 inc = False
+#             if nums[j] < nums[j-1]:
+#                 dec = False
+#             i+=1
+#         return (inc or dec)
 
 s = Solution()
-print(s.isMonotonic([1,2,2,3]))
-print(s.isMonotonic([6,5,4,4]))
-print(s.isMonotonic([1,3,2]))
+# print(s.isMonotonic([1,2,2,3]))
+# print(s.isMonotonic([6,5,4,4]))
+# print(s.isMonotonic([1,3,2]))
+print(s.isMonotonic([3,1,9]))
