18da2a5e6e9ebd6420c0dccde8e0d36b20bacea6
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-07 16:37:59 -0800

    Improved Add Binary runtime

--- LeetCodeAlgos/addBinary.py
+++ LeetCodeAlgos/addBinary.py
@@ -1,11 +1,11 @@
 # Given two binary strings a and b, return their sum as a binary string.
 
+# Condenced variable for better runtime
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         res, sum = '', 0
-        lenn = -max(len(a), len(b))
 
-        for i in range(-1, lenn-1, -1):
+        for i in range(-1, -max(len(a), len(b))-1, -1):
             if i >= -len(a):
                 sum+=int(a[i])
 
@@ -19,6 +19,26 @@ class Solution:
             res+=str(sum)
         res = ''.join(reversed(res))
         return res
+
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         res, sum = '', 0
+#         lenn = -max(len(a), len(b))
+
+#         for i in range(-1, lenn-1, -1):
+#             if i >= -len(a):
+#                 sum+=int(a[i])
+
+#             if i >= -len(b):
+#                 sum+=int(b[i])
+
+#             res+=str(sum%2)
+#             sum = sum//2
+
+#         if sum == 1:
+#             res+=str(sum)
+#         res = ''.join(reversed(res))
+#         return res
 
 s = Solution()
 print(s.addBinary("11", "1"))
