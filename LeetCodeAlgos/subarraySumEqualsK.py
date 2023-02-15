5501bfa8bdb4bcd04fbdec4a0d3096aec91f2ddd
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-15 13:15:41 -0800

    Complete Subarray Sum Equals K
    Implement HashTable incrementation for better performance

--- LeetCodeAlgos/subarraySumEqualsK.py
+++ LeetCodeAlgos/subarraySumEqualsK.py
@@ -7,14 +7,29 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res = 0
-        for start in range(len(nums)):
-            sum = 0
-            for end in range(start, len(nums)):
-                sum+=nums[end]
-                if sum == k:
-                    res+=1
+        res, sum = 0, 0
+        track = {}
+        track[0] = 1
+        for i in range(len(nums)):
+            sum += nums[i]
+            if sum-k in track:
+                res+=track[sum-k]
+            if sum not in track:
+                track[sum] = 1
+            else:
+                track[sum] += 1
         return res
+
+# class Solution:
+#     def subarraySum(self, nums: List[int], k: int) -> int:
+#         res = 0
+#         for start in range(len(nums)):
+#             sum = 0
+#             for end in range(start, len(nums)):
+#                 sum+=nums[end]
+#                 if sum == k:
+#                     res+=1
+#         return res
 
 s = Solution()
 print(s.subarraySum([1],0))
