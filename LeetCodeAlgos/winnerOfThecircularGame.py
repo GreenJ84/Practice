95efadaf6fa6332ac38aa31c9d3528f54988c4c8
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-22 23:47:37 -0800

    Enhance algorithm for runtime and memory performance

--- LeetCodeAlgos/winnerOfThecircularGame.py
+++ LeetCodeAlgos/winnerOfThecircularGame.py
@@ -7,18 +7,30 @@
     # Else, the last friend in the circle wins the game.
 # Given the number of friends, n, and an integer k, return the winner of the game.
 
+# Significant improvement on runtime and memory usage
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         game = [i+1 for i in range(n)]
         pointer = 0
-        for _ in range(n - 1):
-            for c in range(k):
-                if c == k-1:
-                    game.pop(pointer)
-                    pointer -= 1
-                pointer = (pointer + 1) % len(game)
+        while len(game) > 1:
+            pointer = (pointer + k-1) % len(game)
+            game.pop(pointer)
+            if pointer > len(game)-1:
+                pointer = pointer % len(game)
         return game[0]
+    
+# class Solution:
+#     def findTheWinner(self, n: int, k: int) -> int:
+#         game = [i+1 for i in range(n)]
+#         pointer = 0
+#         for _ in range(n - 1):
+#             for c in range(k):
+#                 if c == k-1:
+#                     game.pop(pointer)
+#                     pointer -= 1
+#                 pointer = (pointer + 1) % len(game)
+#         return game[0]
 
 s = Solution()
-print(s.findTheWinner(5, 2))
+# print(s.findTheWinner(5, 2))
 print(s.findTheWinner(6, 5))
