2d396605b86db8d851b6b382e908b7021769c03e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-02-28 12:15:43 -0800

    First attempt on kth Largest Element In Array | Leetcode Algos
    Passed 36/39 - Brute Force

---
+++ LeetCodeAlgos/kthLargestElementInArray.py
@@ -0,0 +1,22 @@
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
+# You must solve it in O(n) time complexity.
+
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        hold = nums[:k]
+        for i in range(k, len(nums)):
+            mn = min(hold)
+            if nums[i] <= mn:
+                continue
+            else:
+                hold.remove(mn)
+                hold.append(nums[i])
+        return min(hold)
+
+s = Solution()
+print(s.findKthLargest([3,2,1,5,6,4], 2))
+print(s.findKthLargest([3,2,3,1,2,4,5,5,6], 4))
