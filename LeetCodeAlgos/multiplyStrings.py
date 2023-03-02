87f1f800054f9b83aba18b4be2717465b7507fce
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-01 20:14:35 -0800

    Add comments to understand algorithm

--- LeetCodeAlgos/multiplyStrings.py
+++ LeetCodeAlgos/multiplyStrings.py
@@ -3,26 +3,32 @@
 
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        # easy out for zero value
         if num1=="0" or num2=="0":
             return "0"
+        # Create a temp answer array and product variable
         temp, prod = [], 0
-
+        # Multiple right to left
         for i in range(-1, -len(num2)-1, -1):
             for j in range(-1, -len(num1)-1, -1):
+                # Product calculation
                 prod = int(num2[i]) * int(num1[j]) + prod
-
+                # Handle the algorithm ending
                 if abs(i) == 1 or -j-i-2>=len(temp):
                     temp.append(prod%10)
-
+                # Add the product smallest digit to the temp
                 else:
                     prod+=temp[-j-i-2]
                     temp[-j-i-2] = prod%10
-
+                # trim the last digit
                 prod=prod//10
+            # Add the remaining product to the temp
             if prod != 0: 
                 temp.append(prod)
                 prod=prod//10
+        # Result string variable
         res = ''
+        # Reverse, Map and Add temporary to res string 
         for i in range(len(temp)-1, -1, -1):
             res+=str(temp[i])
         return res
