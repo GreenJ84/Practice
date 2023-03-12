1e7b9fc3cc52ab5ae8c8a639edf903a31a3df06c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-11 18:10:32 -0800

    Implement a different dps approach checking each coin at a level instad of each frequency of the current coin

--- LeetCodeAlgos/coinChange.py
+++ LeetCodeAlgos/coinChange.py
@@ -10,26 +10,52 @@ class Solution:
         n = len(coins)
         
         @lru_cache(None)
-        def dps(idx, amount):
+        def dps(amount):
             if amount == 0:
                 return 0
             # Return -1 for invalid searches
-            if idx >= n or amount < 0:
+            if amount < 0:
                 return -1
             # Set a minumim coins needed
             min_coins = float('inf')
-            # Search from 0 to the largest possible amount of coins possible at this index
-            for x in range(0, amount // coins[idx] + 1):
-                # Get the optimal amount of coins for the next idx
-                next = dps(idx + 1, amount-x*coins[idx])
-                # Make sure the next ind was valid
+            # Try each coin in the iteration
+            for coin in coins:
+                # Search the next set
+                next = dps(amount - coin)
+                # Make sure the next set was valid
                 if next != -1:
                     # Choose the optimal of current 
-                    min_coins = min(min_coins, next+x)
+                    min_coins = min(min_coins, next + 1)
             # If the current coins werent able to fit, return -1
             return -1 if min_coins == float('inf') else min_coins
         # Start searching from the fisrt coin
-        return dps(0, amount)
+        return dps(amount)
+
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         n = len(coins)
+
+#         @lru_cache(None)
+#         def dps(idx, amount):
+#             if amount == 0:
+#                 return 0
+#             # Return -1 for invalid searches
+#             if idx >= n or amount < 0:
+#                 return -1
+#             # Set a minumim coins needed
+#             min_coins = float('inf')
+#             # Search from 0 to the largest possible amount of coins possible at this index
+#             for x in range(0, amount // coins[idx] + 1):
+#                 # Get the optimal amount of coins for the next idx
+#                 next = dps(idx + 1, amount-x*coins[idx])
+#                 # Make sure the next ind was valid
+#                 if next != -1:
+#                     # Choose the optimal of current 
+#                     min_coins = min(min_coins, next+x)
+#             # If the current coins werent able to fit, return -1
+#             return -1 if min_coins == float('inf') else min_coins
+#         # Start searching from the fisrt coin
+#         return dps(0, amount)
 
 # class Solution:
 #     def coinChange(self, coins: List[int], amount: int) -> int:
