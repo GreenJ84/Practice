2fa1d4ac11d26390f32256be0b8af85286bdb1a8
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-13 10:15:01 -0700

    Complete Maximum Product Subarray | LeetCode Algo

--- LeetCodeAlgos/maximumProductSubarray.py
+++ LeetCodeAlgos/maximumProductSubarray.py
@@ -3,23 +3,46 @@
 
 from typing import List
 
-
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        ans = -float('inf')
-        for start in range(0, len(nums)):
-            mx = nums[start]
+        # hold max and min values
+        # min * a negative will be a max
+        mn, mx = 1, 1
+        # start with the max single value
+        ans = max(nums)
+        # iterate nums
+        for num in nums:
+            # product of zero is always zero / reset
+            if num == 0:
+                mx, mn = 1, 1
+                continue
+            # hold min value
+            temp = mn * num
+            # find new min
+            mn = min(mn*num, mx*num, num)
+            # find new max
+            mx = max(temp, mx*num, num)
+            # is max larger? 
             ans = max(ans, mx)
-            for end in range(start+1, len(nums)):
-                mx *= nums[end]
-                ans = max(ans, mx)
         return ans
+
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         ans = -float('inf')
+#         for start in range(0, len(nums)):
+#             mx = nums[start]
+#             ans = max(ans, mx)
+#             for end in range(start+1, len(nums)):
+#                 mx *= nums[end]
+#                 ans = max(ans, mx)
+#         return ans
 
 s = Solution()
 print(s.maxProduct([2,3,-2,4]))
 print(s.maxProduct([-2,0,-1]))
 print(s.maxProduct([-2]))
 print(s.maxProduct([-3, 0, 1, -2]))
+print(s.maxProduct([2,-5,-2,-4,3]))
 
 
 
