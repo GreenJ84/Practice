d8156884d293858bef8f4cd229a8954d825ef062
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-14 20:44:59 -0700

    Update algorithm for increase in Runtime and Memory

--- LeetCodeAlgos/sameTree.py
+++ LeetCodeAlgos/sameTree.py
@@ -11,45 +11,56 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# signigicant Runtime and Memory performance
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
         if not p or not q:
             return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
 
-        lf, ln = [p], []
-        rt, rn = [q], []
-        while lf and rt:
-            if len(lf)!=len(rt):
-                return False
+# class Solution:
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         if not p and not q:
+#             return True
+#         if not p or not q:
+#             return False
 
-            for i in range(len(lf)):
-                if lf[i].val != rt[i].val:
-                    return False
+#         lf, ln = [p], []
+#         rt, rn = [q], []
+#         while lf and rt:
+#             if len(lf)!=len(rt):
+#                 return False
 
-                if lf[i].left and rt[i].left:
-                    ln.append(lf[i].left)
-                    rn.append(rt[i].left)
-                elif not lf[i].left and not rt[i].left:
-                    pass
-                else:
-                    return False
+#             for i in range(len(lf)):
+#                 if lf[i].val != rt[i].val:
+#                     return False
 
-                if lf[i].right and rt[i].right:
-                    ln.append(lf[i].right)
-                    rn.append(rt[i].right)
-                elif not lf[i].right and not rt[i].right:
-                    pass
-                else:
-                    return False
-            lf, ln = ln, []
-            rt, rn = rn, []
+#                 if lf[i].left and rt[i].left:
+#                     ln.append(lf[i].left)
+#                     rn.append(rt[i].left)
+#                 elif not lf[i].left and not rt[i].left:
+#                     pass
+#                 else:
+#                     return False
 
-        if lf or rt:
-            return False
-        else:
-            return True 
+#                 if lf[i].right and rt[i].right:
+#                     ln.append(lf[i].right)
+#                     rn.append(rt[i].right)
+#                 elif not lf[i].right and not rt[i].right:
+#                     pass
+#                 else:
+#                     return False
+#             lf, ln = ln, []
+#             rt, rn = rn, []
+
+#         if lf or rt:
+#             return False
+#         else:
+#             return True 
 
 s = Solution()
 print(s.isSameTree(
