ead2c46be66fc9fd41f4965f3bee9966565f70a0
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-20 13:00:54 -0700

    implement developed functionality instead of built-in functionality

--- LeetCodeAlgos/searchInRotatedArray.py
+++ LeetCodeAlgos/searchInRotatedArray.py
@@ -5,10 +5,44 @@
 
 from typing import List
 
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target not in nums:
-            return -1
-        else:
-            return nums.index(target)
+        low, high = min(nums), max(nums)
+        res = [-1]
+        def srch(start, end):
+            m = start+((end-start)//2)
+            if nums[start] == target:
+                res.append(start)
+                return
+            if nums[end] == target:
+                res.append(end)
+                return
+            if end == start or start > end: return
+            if nums[m] == target:
+                res.append(m)
+            elif nums[m] < target:
+                if nums[m] < nums[end] and nums[end] < target:
+                    srch(start, m-1)
+                else:
+                    srch(m+1, end)
+            else:
+                if nums[start] < nums[m] and nums[start] > target:
+                    srch(m+1, end)
+                else:
+                    srch(start, m-1)
+        srch(0, len(nums)-1)
+        return res[-1]
+
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if target not in nums:
+#             return -1
+#         else:
+#             return nums.index(target)
+
+s = Solution()
+# print(s.search([4,5,6,7,0,1,2], 0))
+# print(s.search([4,5,6,7,0,1,2], 3))
+# print(s.search([4,5,6,7,8,1,2,3], 8))
+# print(s.search([1], 0))
+print(s.search([5,1,2,3,4], 1))
