e2d4f7ed03dd00376b6c174e447a0cc3d1e6f8bd
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-23 04:01:54 -0700

    Complete Container With Most Water | Leetcode Algo

--- LeetCodeAlgos/containerWithMostWater.py
+++ LeetCodeAlgos/containerWithMostWater.py
@@ -5,17 +5,33 @@
 
 from typing import List
 
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        n = len(height)
         ans = 0
-        for i in range(n):
-            lvlMax = 0
-            for j in range(i+1, n):
-                lvlMax = max(lvlMax, (j-i)*min(height[i], height[j]))
-            ans = max(ans, lvlMax)
+        start, end = 0, len(height)-1
+
+        while start < end:
+            ans = max(ans, min(height[start], height[end])*(end-start))
+            if height[start] < height[end]:
+                start += 1
+                while height[start] < height[start-1]:
+                    start += 1
+            else:
+                end -= 1
+                while height[end] < height[end+1]:
+                    end -= 1
         return ans
+
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         n = len(height)
+#         ans = 0
+#         for i in range(n):
+#             lvlMax = 0
+#             for j in range(i+1, n):
+#                 lvlMax = max(lvlMax, (j-i)*min(height[i], height[j]))
+#             ans = max(ans, lvlMax)
+#         return ans
 
 s = Solution()
 print(s.maxArea([1,8,6,2,5,4,8,3,7]))
