e6df49e086f154184b65ea3284a61209e589709b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-23 21:40:04 -0700

    Cleaned solution for Find Anagrams In String

--- LeetCodeAlgos/findAnagramsInString.py
+++ LeetCodeAlgos/findAnagramsInString.py
@@ -1,23 +1,26 @@
 # Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-from typing import Counter, List
+from typing import List
+from collections import Counter
 
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n1,n2=len(s),len(p)
-        d1=Counter(p)
-        d2=Counter(s[:n2-1])
+        n, m = len(s), len(p)
+        # Hold the count for substring
+        ref = Counter(p)
+        # Hold the count for the current window
+        curr = Counter(s[:m])
         ans=[]
-        j=0
-        for i in range(n2-1,n1):
-            d2[s[i]]+=1
-            if d1==d2:
-                ans.append(j)
-            d2[s[j]]-=1
-            if d2[s[j]]==0:
-                del d2[s[j]]
-            j+=1
+        if curr == ref:
+            ans.append(0)
+
+        # iterate the window
+        for i in range(m,n):
+            curr[s[i]] += 1
+            curr[s[i-m]] -= 1
+            if curr == ref:
+                ans.append(i-m+1)
         return ans
 
 s = Solution()
