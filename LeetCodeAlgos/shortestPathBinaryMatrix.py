664e327efa2a756a98dc672ec349470463a2de90
Jesse Greenough <jesse.greenough84@gmail.com>
2023-03-30 21:54:06 -0700

    Complete Shortest Path in Binary Matrix | Leetcode Algo

--- LeetCodeAlgos/shortestPathBinaryMatrix.py
+++ LeetCodeAlgos/shortestPathBinaryMatrix.py
@@ -4,60 +4,97 @@
 # All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
 # The length of a clear path is the number of visited cells of this path.
 
+from collections import deque
 from typing import List
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        # No starting/end point
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+        # If start or end blocked, quit
+        if grid[0][0] != 0 or grid[-1][-1] != 0:
             return -1
-        minPath = [float('inf')]
-        seen = []
+        n = len(grid)
+        # Presets to produce a full matrix adjacents view
+        offset = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-        def dps(x, y, path, seen):
-            # stop current search if grid is not open
-            if [x, y] in seen:
-                return
-            seen.append([x,y])
-            # if is, increase current path
-            path += 1
-            # if at end check path
-            if x == 0 and y == 0:
-                minPath[0] = min(minPath[0], path)
-                return
-            # If shorter path already stop curr search
-            if minPath[0] <= path:
-                return
-            tX, tY = x-1, y-1
-            bX, bY = x+1, y+1
-            # top
-            if tX >= 0 and grid[tX][y] == 0:
-                dps(tX, y, path, seen[:])
-            # top left
-            if tX >= 0 and tY >= 0 and grid[tX][tY] == 0:
-                dps(tX, tY, path, seen[:])
-            # left
-            if tY >= 0 and grid[x][tY] == 0:
-                dps(x, tY, path, seen[:])
-            # bottom left
-            if bX < n and tY >= 0 and grid[bX][tY] == 0:
-                dps(bX, tY, path, seen[:])
-            # bottom
-            if bX < n and grid[bX][y] == 0:
-                dps(bX, y, path, seen[:])
-            # bottom right
-            if bX < n and bY < n and grid[bX][bY] == 0:
-                dps(bX, bY, path, seen[:])
-            # right
-            if bY < n and grid[x][bY] == 0:
-                dps(x, bY, path, seen[:])
-            # top right
-            if tX >= 0 and bY < n and grid[tX][bY] == 0:
-                dps(tX, bY, path, seen[:])
-        dps(n-1, n-1, 0, seen)
+        q = deque([(0, 0)])
+        seen = set([(0, 0)])
 
-        return minPath[0] if minPath[0] != float('inf') else -1
+        def get_neighbors(_row, _col):
+            for xOff, yOff in offset:
+                newRow, newCol = _row + xOff, _col + yOff
+                if (0 <= newRow < n and 
+                    0 <= newCol < n and
+                    grid[newRow][newCol] == 0 and
+                    (newRow, newCol) not in seen):
+                    yield (newRow, newCol)
+
+        curr = 1
+
+        while q:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+
+                if row == n-1 and col == n-1:
+                    return curr
+
+                for p in get_neighbors(row, col):
+                    seen.add(p)
+                    q.append(p)
+            curr += 1
+        return -1
+
+#! 61 / 89 Test cases passed
+# class Solution:
+#     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+#         n = len(grid)
+#         # No starting/end point
+#         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+#             return -1
+#         minPath = [float('inf')]
+#         seen = []
+
+#         def dps(x, y, path, seen):
+#             # stop current search if grid is not open
+#             if [x, y] in seen:
+#                 return
+#             seen.append([x,y])
+#             # if is, increase current path
+#             path += 1
+#             # if at end check path
+#             if x == 0 and y == 0:
+#                 minPath[0] = min(minPath[0], path)
+#                 return
+#             # If shorter path already stop curr search
+#             if minPath[0] <= path:
+#                 return
+#             tX, tY = x-1, y-1
+#             bX, bY = x+1, y+1
+#             # top
+#             if tX >= 0 and grid[tX][y] == 0:
+#                 dps(tX, y, path, seen[:])
+#             # top left
+#             if tX >= 0 and tY >= 0 and grid[tX][tY] == 0:
+#                 dps(tX, tY, path, seen[:])
+#             # left
+#             if tY >= 0 and grid[x][tY] == 0:
+#                 dps(x, tY, path, seen[:])
+#             # bottom left
+#             if bX < n and tY >= 0 and grid[bX][tY] == 0:
+#                 dps(bX, tY, path, seen[:])
+#             # bottom
+#             if bX < n and grid[bX][y] == 0:
+#                 dps(bX, y, path, seen[:])
+#             # bottom right
+#             if bX < n and bY < n and grid[bX][bY] == 0:
+#                 dps(bX, bY, path, seen[:])
+#             # right
+#             if bY < n and grid[x][bY] == 0:
+#                 dps(x, bY, path, seen[:])
+#             # top right
+#             if tX >= 0 and bY < n and grid[tX][bY] == 0:
+#                 dps(tX, bY, path, seen[:])
+#         dps(n-1, n-1, 0, seen)
+
+#         return minPath[0] if minPath[0] != float('inf') else -1
 
 s = Solution()
 # print(s.shortestPathBinaryMatrix([[0,1],[1,0]]))
