26547f38c81452c76c30b165767c7239dbf1540e
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-06 13:35:04 -0700

    Complete Generate Parenthesis | LeetCode Algo

--- LeetCodeAlgos/generateParenthesis.py
+++ LeetCodeAlgos/generateParenthesis.py
@@ -2,33 +2,50 @@
 
 from typing import List
 
-## First brute force solution
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        comp = n*2
-
         if n == 0: return ans
 
-        def addParens(rang, curr, diff):
-            stk = []
-            for _ in range(rang):
-                curr += "("
-                stk.append(")")
-            while stk:
-                curr += stk.pop()
+        def backtrack(s: str, open: int, close: int):
+            if len(s) == 2*n:
+                ans.append(s)
+                return
+            if open < n:
+                backtrack(s+'(', open + 1, close)
+            if close < open:
+                backtrack(s+')', open, close+1)
 
-            if len(curr) == n*2:
-                ans.append(curr)
-            else:
-                x = comp-len(curr)
-                rang = x//2
-                for i in range(rang):
-                    addParens(i+1, curr[:])
-            
-        for i in range(n):
-            addParens(i+1, "", 0)
+        backtrack("", 0, 0)
         return ans
+
+## First brute force solution
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         ans = []
+#         comp = n*2
+
+#         if n == 0: return ans
+
+#         def addParens(rang, curr, diff):
+#             stk = []
+#             for _ in range(rang):
+#                 curr += "("
+#                 stk.append(")")
+#             while stk:
+#                 curr += stk.pop()
+
+#             if len(curr) == n*2:
+#                 ans.append(curr)
+#             else:
+#                 x = comp-len(curr)
+#                 rang = x//2
+#                 for i in range(rang):
+#                     addParens(i+1, curr[:])
+            
+#         for i in range(n):
+#             addParens(i+1, "", 0)
+#         return ans
 
 s = Solution()
 print(s.generateParenthesis(3))
