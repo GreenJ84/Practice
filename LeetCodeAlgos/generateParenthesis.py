2a1ec6bab097b00cf786526a6a9bd8cf59725988
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-06 13:31:18 -0700

    Generate Parenthesis Brute force solution

---
+++ LeetCodeAlgos/generateParenthesis.py
@@ -0,0 +1,37 @@
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+from typing import List
+
+## First brute force solution
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        comp = n*2
+
+        if n == 0: return ans
+
+        def addParens(rang, curr, diff):
+            stk = []
+            for _ in range(rang):
+                curr += "("
+                stk.append(")")
+            while stk:
+                curr += stk.pop()
+
+            if len(curr) == n*2:
+                ans.append(curr)
+            else:
+                x = comp-len(curr)
+                rang = x//2
+                for i in range(rang):
+                    addParens(i+1, curr[:])
+            
+        for i in range(n):
+            addParens(i+1, "", 0)
+        return ans
+
+s = Solution()
+print(s.generateParenthesis(3))
+print(s.generateParenthesis(2))
+print(s.generateParenthesis(1))
+print(s.generateParenthesis(0))
