001d42d1db11c3c993d455bdbdb28d08cd6176c0
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-08 18:50:57 -0700

    Bitwise AND | 8268 out of 8269 test cases passed

---
+++ LeetCodeAlgos/bitwiseAnd.py
@@ -0,0 +1,21 @@
+# Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+import math
+
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        if left == right or left == 0: return left
+
+        l,r = math.floor(math.log(left, 2)), math.floor(math.log(right, 2))
+        if l!=r: return 0
+
+        for num in range(left+1, right+1):
+            left = left & num
+
+        return left
+    
+s = Solution()
+print(s.rangeBitwiseAnd(5, 7))
+print(s.rangeBitwiseAnd(0, 0))
+print(s.rangeBitwiseAnd(1, 2147483646))
