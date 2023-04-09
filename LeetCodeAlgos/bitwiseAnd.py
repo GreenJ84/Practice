0e8e33239422bc21b9a0984b85504e636ab255fc
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-08 18:53:44 -0700

    Complete Bitwise AND | LeetCode Algos

--- LeetCodeAlgos/bitwiseAnd.py
+++ LeetCodeAlgos/bitwiseAnd.py
@@ -1,19 +1,28 @@
 # Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
 
-import math
-
 
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        if left == right or left == 0: return left
+        shift = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        return left << shift
 
-        l,r = math.floor(math.log(left, 2)), math.floor(math.log(right, 2))
-        if l!=r: return 0
 
-        for num in range(left+1, right+1):
-            left = left & num
+# import math
+# class Solution:
+#     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+#         if left == right or left == 0: return left
 
-        return left
+#         l,r = math.floor(math.log(left, 2)), math.floor(math.log(right, 2))
+#         if l!=r: return 0
+
+#         for num in range(left+1, right+1):
+#             left = left & num
+
+#         return left
     
 s = Solution()
 print(s.rangeBitwiseAnd(5, 7))
