12502e5e5c5857c3e7a65cd8764bec80241a9d28
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-09 08:30:59 -0700

    Complete Word Break | LeetCode Algo

--- LeetCodeAlgos/wordBreak.py
+++ LeetCodeAlgos/wordBreak.py
@@ -3,17 +3,30 @@
 
 from typing import List
 
-
-class Solution:
+class Solution():
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        window = ""
-        for char in s:
-            window += char
-            if window in wordDict:
-                s = s.replace(window, "")
-                window = ""
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[0] = True
         
-        return True if not s else False
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         window = ""
+#         for char in s:
+#             window += char
+#             if window in wordDict:
+#                 s = s.replace(window, "")
+#                 window = ""
+        
+#         return True if not s else False
     
 s = Solution()
 print(s.wordBreak("leetcode", ["leet","code"]))
