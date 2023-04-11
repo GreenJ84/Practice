4c92a99e945cbc32851847bf78fb5ff5f4b11d41
Jesse Greenough <jesse.greenough84@gmail.com>
2023-04-10 17:52:22 -0700

    Imporve algorithm solution for performance

--- LeetCodeAlgos/removeStarFromString.py
+++ LeetCodeAlgos/removeStarFromString.py
@@ -6,26 +6,36 @@
 # Note:
     # The input will be generated such that the operation is always possible.
     # It can be shown that the resulting string will always be unique.
-
 class Solution:
     def removeStars(self, s: str) -> str:
-        n = len(s)
-        if s.count('*') > n/2: 
-            return ''
+        ans = []
+        for char in s:
+            if char == '*':
+                if ans:
+                    ans.pop()
+                continue
+            ans.append(char)
+        return ''.join(ans)
 
-        while s[0] == '*':
-            s = s[1:]
-            n -= 1
+# class Solution:
+#     def removeStars(self, s: str) -> str:
+#         n = len(s)
+#         if s.count('*') > n/2: 
+#             return ''
 
-        char = 0
-        while char < n-1:
-            if s[char+1] == '*':
-                s = s[:char]+s[char+2:]
-                char -= 1
-                n -= 2
-            else:
-                char += 1
-        return s
+#         while s[0] == '*':
+#             s = s[1:]
+#             n -= 1
+
+#         char = 0
+#         while char < n-1:
+#             if s[char+1] == '*':
+#                 s = s[:char]+s[char+2:]
+#                 char -= 1
+#                 n -= 2
+#             else:
+#                 char += 1
+#         return s
 
 s = Solution()
 print(s.removeStars("leet**cod*e"))
