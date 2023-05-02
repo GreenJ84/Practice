9ee433e1d23c52526ea38fbb5dbf6ab61843a43b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-02 14:18:54 -0700

    Improve function performance

--- LeetCodeAlgos/signOfProductOfArray.py
+++ LeetCodeAlgos/signOfProductOfArray.py
@@ -13,7 +13,18 @@ class Solution:
     def arraySign(self, nums: List[int]) -> int:
         if 0 in nums:
             return 0
-        return 1 if functools.reduce(lambda x, y: x*y, nums) > 0 else -1
+        negs = 0
+        for i in nums:
+            if i < 0:
+                negs += 1
+        
+        return -1 if negs % 2 == 1 else 1
+
+# class Solution:
+#     def arraySign(self, nums: List[int]) -> int:
+#         if 0 in nums:
+#             return 0
+#         return 1 if functools.reduce(lambda x, y: x*y, nums) > 0 else -1
 
 
 s = Solution()
