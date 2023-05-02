34fc5cc887def5f1e69e4c4103fe6f07c73c252c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-02 14:11:20 -0700

    Compplete count special quadruplets

--- LeetCodeAlgos/countSpecialQuadruplets.py
+++ LeetCodeAlgos/countSpecialQuadruplets.py
@@ -7,12 +7,11 @@ from typing import List
 
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
         count = 0
-        for a in range(n):
-            for b in range(a+1, n):
-                for c in range(b+1, n):
+        for a in range(n-3):
+            for b in range(a+1, n-2):
+                for c in range(b+1, n-1):
                     for d in range(c+1, n):
                         if nums[a] + nums[b] + nums[c] == nums[d]:
                             count += 1
