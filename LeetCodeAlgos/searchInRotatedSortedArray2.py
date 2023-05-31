6d86d844f07d52e61bb5e6264fbc325846e85637
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-31 02:39:55 -0700

    Complete Search In Rotated Sorted Array 2 | LeetCode Algo

---
+++ LeetCodeAlgos/searchInRotatedSortedArray2.py
@@ -0,0 +1,34 @@
+# There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+# Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+# Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+# You must decrease the overall operation steps as much as possible.
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        nums = list(set(nums))
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return True
+            elif nums[mid] <= nums[right]:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if nums[mid] >= target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return False
+    
+
+s = Solution()
+print(s.search([2,5,6,0,0,1,2], 0))
+print(s.search([2,5,6,0,0,1,2], 3))
+print(s.search([1,0,1,1,1], 0))
+print(s.search([1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1], 2))
