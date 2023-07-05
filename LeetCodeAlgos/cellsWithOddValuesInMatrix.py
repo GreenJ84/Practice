e1e6c7da5da1dc5c5075d94ef44ec5bfb091acf2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-07-05 12:34:02 -0700

    Refactor Cells With Odd Values In Matrix for performance

--- LeetCodeAlgos/cellsWithOddValuesInMatrix.py
+++ LeetCodeAlgos/cellsWithOddValuesInMatrix.py
@@ -9,30 +9,42 @@
 
 from typing import List
 
-
 class Solution:
     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
-        matrix = [[0 for _ in range(n)] for _ in range(m)]
-        print(matrix)
-
-        def incrementRow(rowIdx):
-            matrix[rowIdx] = list( map(lambda x: x + 1, matrix[rowIdx]) )
-
-        def incrementCol(colIdx):
-            for row in range(0, m):
-                matrix[row][colIdx] = matrix[row][colIdx] + 1
-
+        # Odd holder for each row
+        odd_rows = [False for _ in range(m)]
+        # Odd holder for each coll
+        odd_cols = [False for _ in range(n)]
+        # Indecis updates
         for row, col in indices:
-            incrementRow(row)
-            incrementCol(col)
-            print(matrix)
+            odd_rows[row] ^= True
+            odd_cols[col] ^= True
+        # Sum of odd values for each col in each row
+        # Matrix mapping row to col summation
+        return sum (oR ^ oC for oR in odd_rows for oC in odd_cols)
 
-        return sum(
-            map(
-                lambda row: sum(1 for _ in filter(lambda item: item % 2 == 1, row)),
-                matrix
-            )
-        )
+# class Solution:
+#     def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+#         matrix = [[0 for _ in range(n)] for _ in range(m)]
+
+#         def incrementRow(rowIdx):
+#             matrix[rowIdx] = list( map(lambda x: x + 1, matrix[rowIdx]) )
+
+#         def incrementCol(colIdx):
+#             for row in range(0, m):
+#                 matrix[row][colIdx] = matrix[row][colIdx] + 1
+
+#         for row, col in indices:
+#             incrementRow(row)
+#             incrementCol(col)
+#             print(matrix)
+
+#         return sum(
+#             map(
+#                 lambda row: sum(1 for _ in filter(lambda item: item % 2 == 1, row)),
+#                 matrix
+#             )
+#         )
     
 s = Solution()
 print(s.oddCells(2, 3, [[0,1],[1,1]]))
