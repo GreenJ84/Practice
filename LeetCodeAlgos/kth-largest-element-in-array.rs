f0942cfc44f802e4cdd9913a9651fd40d5311ea6
Jesse Greenough <jesse.greenough84@gmail.com>
2023-08-14 09:36:55 -0700

    Update algorithm to be non consuming utilizing Binary Heap

--- LeetCodeAlgos/kth-largest-element-in-array.rs
+++ LeetCodeAlgos/kth-largest-element-in-array.rs
@@ -3,12 +3,27 @@
 // Can you solve it without sorting?
 
 struct Solution {}
+// TC: O(n) + k, SC: 0(n). Non Vec Consuming
+use std::collections::BinaryHeap;
 impl Solution {
-    pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
-        nums.sort_unstable_by(|a, b| b.cmp(a));
-        nums[k as usize - 1]
+    pub fn find_kth_largest(nums: Vec<i32>, mut k: i32) -> i32 {
+        let mut copy = BinaryHeap::from(nums);
+        return loop {
+            k -= 1;
+            if k == 0 {
+                break copy.pop().unwrap();
+            }
+            copy.pop().unwrap();
+        }
     }
 }
+// TC: 0(n), SC: 0. Vec Consuming
+// impl Solution {
+//     pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+//         nums.sort_unstable_by(|a, b| b.cmp(a));
+//         nums[k as usize - 1]
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
