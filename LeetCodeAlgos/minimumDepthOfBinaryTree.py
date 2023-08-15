e4362f903422c7bd268275c41449dc1154499443
Jesse Greenough <jesse.greenough84@gmail.com>
2023-08-15 08:03:59 -0700

    Enhance algorithm performance
    Implement a BFS instead of a DFS algorithm, and implement a Deque structure instead of recursion.

--- LeetCodeAlgos/minimumDepthOfBinaryTree.py
+++ LeetCodeAlgos/minimumDepthOfBinaryTree.py
@@ -13,14 +13,31 @@ class TreeNode:
         self.left = left
         self.right = right
         
+
+from collections import deque
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        if root.left is None and root.right is None:
-            return 1
-        
-        minLeft = self.minDepth(root.left) if root.left else float('inf')
-        minRight = self.minDepth(root.right) if root.right else float('inf')
+        if not root: return 0
+        if not root.left and not root.right: return 1
+        queue = deque([(root, 1)])
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
 
-        return 1 + min(minLeft, minRight)
+
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if root is None:
+#             return 0
+#         if root.left is None and root.right is None:
+#             return 1
+        
+#         minLeft = self.minDepth(root.left) if root.left else float('inf')
+#         minRight = self.minDepth(root.right) if root.right else float('inf')
+
+#         return 1 + min(minLeft, minRight)
