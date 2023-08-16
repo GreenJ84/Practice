46e77674061d50cc8a70e63afbf7a942ea04bb11
Jesse Greenough <jesse.greenough84@gmail.com>
2023-08-16 01:10:23 -0700

    Complete Find Subarrays With Equal Sum | LeetCode Algos Python

---
+++ LeetCodeAlgos/findSubarraysWithEqualSum.py
@@ -0,0 +1,23 @@
+# Given a 0-indexed integer array nums, determine whether there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
+
+# Return true if these subarrays exist, and false otherwise.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+from typing import List
+
+
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        if len(nums) < 2:
+            return False
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
+                if nums[i] + nums[i + 1] == nums[j] + nums[j + 1]:
+                    return True
+        return False
+    
+s = Solution()
+print(s.findSubarrays([4,2,4]))
+print(s.findSubarrays([1,2,3,4,5]))
+print(s.findSubarrays([0,0,0]))
