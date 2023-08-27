876d0d4b9f65cc0e865c080727d30312d779ed48
Jesse Greenough <jesse.greenough84@gmail.com>
2023-08-27 09:52:36 -0700

    Refine algorithm performance

--- LeetCodeAlgos/timeoutCancelation.js
+++ LeetCodeAlgos/timeoutCancelation.js
@@ -3,8 +3,18 @@
 // After a delay of t, fn should be called with args passed as parameters unless cancelFn was invoked before the delay of t milliseconds elapses, specifically at cancelT ms. In that case, fn should never be called.
 
 const cancellable = function(fn, args, t) {
-    
-  const timeout = setTimeout(fn(...args), t);
+  let cancel = false;
+  setTimeout(() => {
+    if (!cancel) { fn(...args) }
+  }, t);
 
-  return () => clearTimeout(timeout);
+  return () => { cancel = true; };
 };
+
+
+// const cancellable = function(fn, args, t) {
+    
+//   const timeout = setTimeout(fn(...args), t);
+
+//   return () => clearTimeout(timeout);
+// };
