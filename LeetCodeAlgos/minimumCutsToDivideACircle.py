2576b0b889a79ba33a1aa4c18391944131382127
Jesse Greenough <jesse.greenough84@gmail.com>
2023-09-05 00:52:50 -0700

    Complete minimumCutsToDivideACircle | LeetCode Algos Python

---
+++ LeetCodeAlgos/minimumCutsToDivideACircle.py
@@ -0,0 +1,10 @@
+# A valid cut in a circle can be:
+
+# A cut that is represented by a straight line that touches two points on the edge of the circle and passes through its center, or
+# A cut that is represented by a straight line that touches one point on the edge of the circle and its center.
+# Some valid and invalid cuts are shown in the figures below.
+
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        if n == 1: return 0
+        return n if n % 2 == 1 else n // 2
