364cba67dad0f42654763513cce495bdf60ec000
Jesse Greenough <jesse.greenough84@gmail.com>
2023-10-01 18:58:02 -0700

    Simplify algorithm logic, add to function testing

--- LeetCodeAlgos/mergeStringsAlternately.py
+++ LeetCodeAlgos/mergeStringsAlternately.py
@@ -5,34 +5,27 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res = ''
         w1, w2 = len(word1), len(word2)
-        for i in range(0, max(w1, w2)):
-            if i < w1: 
-                res+=word1[i]
-            else: 
-                res+=word2[i:]
-                break
-            if i < w2: 
-                res+=word2[i]
-            else: 
-                res+=word1[i+1:]
-                break
+        for i in range(min(w1, w2)):
+            res += word1[i]
+            res += word2[i]
+        if w1 < w2:
+            res += word2[w1:]
+        elif w1 > w2:
+            res += word1[w2:]
         return res
 
-# class Solution:
-#     def mergeAlternately(self, word1: str, word2: str) -> str:
-#         res = ''
-#         for i in range(0, min(len(word1), len(word2))):
-#             res+=word1[i]
-#             res+=word2[i]
-#         if len(word1)>len(word2):
-#             res+=word1[slice(i+1, len(word1))]
-#         elif len(word1)<len(word2):
-#             res+=word2[slice(i+1, len(word2))]
-#         return res
-
 s = Solution()
-print(s.mergeAlternately('abc','pqr'))
-print(s.mergeAlternately('ab','pqrs'))
-print(s.mergeAlternately('abcd','pq'))
+
+test1 = s.mergeAlternately('abc','pqr')
+print(test1)
+assert test1 == 'apbqcr'
+
+test2 = s.mergeAlternately('ab','pqrs')
+print(test2)
+assert test2 == 'apbqrs'
+
+test3 = s.mergeAlternately('abcd','pq')
+print(test3)
+assert test3 == 'apbqcd'
 
 
