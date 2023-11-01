37ef83353593a74b0d41eeab4134e41ee7448e83
Jesse Greenough <jesse.greenough84@gmail.com>
2023-10-31 23:41:05 -0700

    Complete Single Number | LeetCode Algorithm Java

---
+++ LeetCodeAlgos/singleNumber.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+      for (int num: nums){
+        ans ^= num;
+      }
+      return ans;
+    }
+}
+
+class singleNumber {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.singleNumber(new int[]{1,2,2}));
+    System.out.println(s.singleNumber(new int[]{4,1,2,1,2}));
+    System.out.println(s.singleNumber(new int[]{1}));
+  }
+}
