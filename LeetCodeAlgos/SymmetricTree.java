2b032869a9d1658490d670d5a7e71efae5ca52ab
Jesse Greenough <jesse.greenough84@gmail.com>
2023-11-05 00:15:48 -0700

    Refine algorithm logic for better runtime

--- LeetCodeAlgos/SymmetricTree.java
+++ LeetCodeAlgos/SymmetricTree.java
@@ -16,42 +16,18 @@ class TreeNode {
 
 class Solution {
   public boolean isSymmetric(TreeNode root) {
-    if (root == null || (root.left == null && root.right == null)) return true;
+    if (root.left == null && root.right == null) return true;
     else if (root.left == null ^ root.right == null) return false;
     else return followTree(root.left, root.right);
   }
+
   public boolean followTree(TreeNode left, TreeNode right) {
     // End of both tree lines
     if (left == null && right == null) return true;
     // If only one tree ends
-    else if (left == null ^ right == null) {
-        System.out.println("fail null match");
+    else if ((left == null ^ right == null) || left.val != right.val) {
         return false;
-      }
-    // Check both current values
-    if (left.val != right.val) {
-        System.out.println("fail value match");
-        return false;
-      }
-    // Outside node checks
-    else if (left.left == null ^ right.right == null) {
-        System.out.println("fail outside null match");
-        return false;
-      }
-    // inside node checks
-    else if (left.right == null ^ right.left == null) {
-        System.out.println("fail inside null match");
-        return false;
-      }
-    else if (!followTree(left.left, right.right)) {
-        System.out.println("fail outside children");
-        return false;
-      }
-    else if (!followTree(left.right, right.left)) {
-        System.out.println("fail inside children");
-        return false;
-      }
-    
+    }
     
     return followTree(left.left, right.right) && followTree(left.right, right.left);
   }
