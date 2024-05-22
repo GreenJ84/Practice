b74802de082756359f36a133f37841725d0a8623
Jesse Greenough <jesse.greenough84@gmail.com>
2024-05-21 22:39:07 -0700

    Complete Categorize Box According To Criteria | LeetCode Algo Java

---
+++ LeetCodeAlgos/Java/CategorizeBoxAccordingToCriteria.java
@@ -0,0 +1,45 @@
+// Given four integers length, width, height, and mass, representing the dimensions and mass of a box, respectively, return a string representing the category of the box.
+  // The box is "Bulky" if:
+    // Any of the dimensions of the box is greater or equal to 104.
+    // Or, the volume of the box is greater or equal to 109.
+  // If the mass of the box is greater or equal to 100, it is "Heavy".
+  // If the box is both "Bulky" and "Heavy", then its category is "Both".
+  // If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+  // If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+  // If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+// Note that the volume of the box is the product of its length, width and height.
+
+public class CategorizeBoxAccordingToCriteria {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    testCategorizeBoxAccordingToCriteria(1, 1000, 35, 700, 300, "Heavy", solution);
+    testCategorizeBoxAccordingToCriteria(2, 200, 50, 800, 50, "Neither", solution);
+    testCategorizeBoxAccordingToCriteria(3, 2909, 3968, 3272, 727, "Both", solution);
+  }
+
+  public static void testCategorizeBoxAccordingToCriteria(int testNum, int length, int width, int height, int mass, String expected, Solution s) {
+    String result = s.categorizeBox(length, width, height, mass);
+
+    System.out.println(String.format(
+      "Test %d: %s / %s (%s)",
+      testNum,
+      result,
+      expected,
+      result.equals(expected) ? "PASS" : "FAIL"
+    ));
+  }
+}
+
+class Solution {
+  public String categorizeBox(int length, int width, int height, int mass) {
+    boolean bulky = length >= 10000 ||
+      width >= 10000 ||
+      height >= 10000 ||
+      (long) length * (long) width * (long) height >= 1000000000;
+    boolean heavy = mass >= 100;
+    return bulky ^ heavy ?
+      bulky ? "Bulky" : "Heavy" :
+      bulky ? "Both": "Neither";
+  }
+}
