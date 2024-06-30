b8e724263223c5f087ae3f6bf3bd459095e039a7
Jesse Greenough <jesse.greenough84@gmail.com>
2024-06-29 20:25:13 -0700

    Complete Implement Stack Using Queues | Leet Code Algo Java

---
+++ LeetCodeAlgos/Java/ImplementStackUsingQueues.java
@@ -0,0 +1,59 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+// Implement the MyStack class:
+  // void push(int x) Pushes element x to the top of the stack.
+  // int pop() Removes the element on the top of the stack and returns it.
+  // int top() Returns the element on the top of the stack.
+  // boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+  // You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+  // Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+
+import java.util.*;
+
+public class ImplementStackUsingQueues {
+  public static void main(String[] args) { }
+}
+
+class MyStack {
+  Queue <Integer> hold;
+  Queue <Integer> last;
+  public MyStack() {
+    hold = new LinkedList<>();
+    last = new LinkedList<>();
+  }
+
+  public void push(int x) {
+    if (!last.isEmpty()){
+      hold.add(last.poll());
+    }
+    last.add(x);
+  }
+
+  public int pop() {
+    int top = last.poll();
+    if (!hold.isEmpty()){
+      for (int items = 0; items < hold.size() - 1; items++){
+        hold.add( hold.poll() );
+      }
+      last.add( hold.poll() );
+    }
+    return top;
+  }
+
+  public int top() {
+    return last.peek();
+  }
+  
+  public boolean empty() {
+    return last.isEmpty();
+  }
+}
+
+/**
+* Your MyStack object will be instantiated and called as such:
+* MyStack obj = new MyStack();
+* obj.push(x);
+* int param_2 = obj.pop();
+* int param_3 = obj.top();
+* boolean param_4 = obj.empty();
+*/
