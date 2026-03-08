9db49d0f4634d64489145e125b195bfafb598d36
Jesse Greenough <jesse.greenough84@gmail.com>
2026-03-07 18:14:00 -0800

    Complete Rectangle Overlap | LeetCode Algo Rust

---
+++ LeetCodeAlgos/Rust/rectangle-overlap.rs
@@ -0,0 +1,43 @@
+// An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
+
+// Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+// Given two axis-aligned rectangles rec1 and rec2, return true if they overlap, otherwise return false.
+
+struct Solution;
+impl Solution {
+    pub fn is_rectangle_overlap(rec1: Vec<i32>, rec2: Vec<i32>) -> bool {
+        // X - overlap
+        if (rec1[0] <= rec2[0] && rec1[2] > rec2[0]) ||
+           (rec1[2] >= rec2[2] && rec1[0] < rec2[2]) ||
+           (rec1[0[] >= rec2[0] && rec1[2] <= rec2[2]) {
+          if (rec1[1] <= rec2[1] && rec1[3] > rec2[1]) ||
+             (rec1[3] >= rec2[3] && rec1[1] < rec2[3]) ||
+             (rec1[1] >= rec2[1] && rec1[3] <= rec2[3]) {
+            return true;
+          }
+        }
+
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_1() {
+    assert_eq!(Solution::is_rectangle_overlap(vec![0,0,2,2], vec![1,1,3,3]), true);
+  }
+
+  #[test]
+  fn test_2() {
+    assert_eq!(Solution::is_rectangle_overlap(vec![0,0,1,1], vec![1,0,2,1]), false);
+  }
+
+  #[test]
+  fn test_3() {
+    assert_eq!(Solution::is_rectangle_overlap(vec![0,0,1,1], vec![2,2,3,3]), false);
+  }
+}
